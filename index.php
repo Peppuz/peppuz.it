0dd135c058f1496926f467dc54f75aa8172b81da
root <root@debian10>
2021-08-12 08:52:32 +0100

    init

---
+++ index.php
@@ -0,0 +1,143 @@
+<?php ?>
+<html style="">
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.typekit.net/jdu5pai.css">
+    <script src="https://unpkg.com/feather-icons"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+
+    <meta name="viewport" content="width=100%, initial-scale=1.0">
+</head>
+
+<body style="color:white">
+    <div class="">
+        <img src="assets/banner.PNG" class="img-fluid text-center" style="position:absolute; padding:0;margin:0; top:0px; opacity:.3; z-index:-1 " alt="">
+        <div class="">
+
+            <!-- TOP -->
+            <div class="my-5 mb-2">
+                <img src="icon.png" class="img-fluid text-center d-block mx-auto" alt="">
+
+                <div class="my-5 text-center">
+                    <h1 class="text-left" style="font-size: 3rem ;font-weight: bold; color:#00ACC1;">P tilde Z</h1>
+                </div>
+
+                <div class="text-center ">
+                    <a class="col-8 col-md-4 btn" style="border:none; border-radius:8px; color:#00Acc1" href="https://play.google.com/store/apps/details?id=svd.stp.manuel.oraristp&hl=en_US&gl=US"> <img src="assets/google-play-badge.png" class="img-fluid"> </a>
+                </div>
+            </div>
+
+            <!-- CODIN -->
+            <div class="text-center col-10 offset-1 my-5 p-5 " style="border-radius: 15px; border: 1px solid transparent; background-color:#00ACC1; box-shadow: 0px 0px 80px #00000029;">
+                <img src="assets/passaggi.svg" class="img-fluid mb-4" alt="">
+                <h3 style="font-size:2rem; letter-spacing: 0px;">Quattro semplici passaggi.</h3>
+                <p class="col-10 offset-1 text-center mt-3">Orari STP semplifica la ricerca dei bus di cui hai bisogno. <br> Dimentica le tabelle PDF. <br> Partenza. Destinazione. Orario. Fermate <br> Tutto qui.</p>
+            </div>
+
+            <!-- MUSIC -->
+            <div class="text-center col-10 offset-1 my-5 p-5 d-md-flex " style="border-radius: 15px; border: 1px solid transparent; background-color:#d7f9fc; box-shadow: 0px 0px 80px #00000029;">
+                <div class="col-12 col-md-4 align-self-center mx-auto">
+                    <h3 class="col-12" style="color:#28c5d1; font-size:2rem"> Presenti su tutti i dispositivi.</h3>
+                    <p class="col-10 offset-1 text-center" style="color:#28c5d1">Con la nuova versione di Orari STP è possibile accedere trammite tutti i dispositivi. <br> A te la scelta </p>
+                </div>
+
+                <div class="col-12 col-md-6">
+                    <img src="assets/immagine.png" class="img-fluid" alt="">
+                </div>
+            </div>
+
+            <div class="text-center col-10 offset-1 my-5 p-5 " style="border-radius: 15px; border: 1px solid transparent; background-color:#00ACC1; box-shadow: 0px 0px 80px #00000029;">
+                <h3 class="pb-4" style="font-size:2rem; letter-spacing: 0px;">Il nostro Team.</h3>
+
+                <div class="row">
+                    <div class="col-12 col-md-4 mb-5  mt-3 mb-lg-0">
+                        <img class="bd-placeholder-img rounded-circle mb-3" width="140" height="140" src="assets/pz.jpeg" role="img" aria-label="" preserveAspectRatio="xMidYMid slice" focusable="false">
+
+                        <h2>Giuseppe Vitale </h2>
+                        <h6>@peppuz_</h6>
+                        <p> Fullstack developer</p>
+                        <div class="row mx-5" style="color:white">
+                            <a class="col" href="https://t.me/pz_boi"> <i style="color:white" data-feather="send"></i> </a>
+                            <a class="col" href="https://instagram.com/peppuz_"> <i style="color:white" data-feather="instagram"></i> </a>
+                            <a class="col" href="https://github.com/Peppuz/"> <i style="color:white" data-feather="github"></i> </a>
+                        </div>
+
+                    </div>
+                    <div class="col-12 col-md-4 mb-5 mt-3 mb-lg-0">
+                        <img class="bd-placeholder-img rounded-circle mb-3" width="140" height="140" src="assets/vorpal.jpeg" role="img" aria-label="" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <h2> Manuel Manelli </h2>
+                        <h6>@vorpal_97</h6>
+                        <p>Backend developer</p>
+                        <div class="row mx-5" style="color:white">
+                            <a class="col" href="https://t.me/Vorpal_97"> <i style="color:white" data-feather="send"></i> </a>
+                            <a class="col" href="https://instagram.com/vorpal_97"> <i style="color:white" data-feather="instagram"></i> </a>
+                            <a class="col" href="https://github.com/Vorpal97"> <i style="color:white" data-feather="github"></i> </a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 mb-5 mt-3 mb-lg-0">
+                        <img class="bd-placeholder-img rounded-circle mb-3" width="140" height="140" src="assets/alberto.jpeg" role="img" aria-label="" preserveAspectRatio="xMidYMid slice" focusable="false">
+
+                        <h2>Alberto Carrone</h2>
+                        <h6>@albertocarrone</h6>
+                        <p>Designer</p>
+                        <div class="row mx-5 " style="color:white">
+                            <a class="col" href="https://t.me/albrtcr"> <i style="color:white" data-feather="send"></i> </a>
+                            <a class="col" href="https://instagram.com/albertocarrone"> <i style="color:white" data-feather="instagram"></i> </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="text-center col-10 offset-1 my-5 p-5 " style="border-radius: 15px; border: 1px solid transparent; background-color:#d7f9fc; box-shadow: 0px 0px 80px #00000029;">
+                <div>
+                    <h3 class="col-12" style="color:#28c5d1; font-size:2rem"> Hai una domanda da farci?</h3>
+                    <p class="col-10 offset-1 text-center" style="color:#28c5d1">Contattaci all'indirizzo di posta elettronica </p>
+                        <br>
+                        <br>
+                        <div style="display: flex;justify-content: center;">
+                          <div style="border:solid 1px #28c5d1; border-radius:15px; width: -moz-fit-content; width: fit-content;" class="p-3">
+                            <i style="color:#28c5d1; margin-right:5px; position:absolute" data-feather="mail"></i>
+                            <a style="color:#00ACC1; text-decoration:solid; margin-left:30" href="mailto:svdevteam72019@gmail.com">svdevteam72019@gmail.com</a>
+                          </div>
+                        </div>
+                        <br>
+                        <br>
+                        <p class="col-10 offset-1 text-center" style="color:#28c5d1">Attenzione, richieste riguardanti il servizio di trasporto verranno ignorate. <br>
+                        Contattate la società responsabile del servizio di trasporto pubblico.
+                    </p>
+                </div>
+
+                <div>
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+
+
+
+        <label class="w-100 text-center" style="color:#b6edf1;font-weight:bold; font-size:1rem">POWERED BY SVDEVTEAM</label>
+        <!--
+        <div class="col-6 offset-3 mt-5 text-justified">
+            <p style="font-size:1rem">
+                Dopo un sacco di vostre mail dove ci chiedevate che fine avesse fatto Orari STP, abbiamo deciso di recuperare il codice e di rimetterlo a vostra disposizione!
+                Orari STP, non per nostro volere, da questo momento torna ad essere un applicazione indipendente per consultare gli orari dei pullman nella provincia di Brindisi. Purtroppo per noi questa applicazione non rappresenta una fonte di guadagno, quindi nel corso dell'ultimo anno abbiamo preferito dare precedenza ai nostri studi e ad altri progetti.
+                Attualmente stiamo terminando le ultime fasi di testing quindi a breve l'applicazione si aggiornerà spontaneamente rivelandoti la nuova versione di Orari STP.
+                Grazie a tutti per le belle parole che ci avete mandato! <br> Manuel, Giuseppe & Alberto
+            </p>
+
+        </div> -->
+        <br>
+        <br>
+        <br>
+    </div>
+    </div>
+
+    <script>
+        feather.replace()
+    </script>
+
+</body>
+
+</html>

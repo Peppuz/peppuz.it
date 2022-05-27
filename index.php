9e5f38ad09655d475ff7c4aa1179aac318f751e9
Peppuz <peppuzvitale@gmail.com>
2022-05-27 19:40:26 +0100

    Adding Spipa, edits on index

--- index.php
+++ index.php
@@ -5,134 +5,60 @@
     <title> P~Z</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.5/lodash.min.js"></script>
+
     <link rel="stylesheet" href="https://use.typekit.net/jdu5pai.css">
+    <link rel="stylesheet" href="/spipa.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="icon" href="/assets/pz_logo_nobg.png">
     <script src="https://unpkg.com/feather-icons"></script>
     <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+    <script src="/spipa.js"></script>
+    <meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
 
-    <meta name="viewport" content="width=100%, initial-scale=1.0">
+    <style> .btn-pz{ color:#fff;background-color:#d7e7ff00;border-color:#e2e6ed;} </style> 
 
-    <style>
-        :root {}
-    </style>
 </head>
 
-<body style="color:black">
-    <div class="">
-        <img src="assets/pz_logo_nobg.png" class="img-fluid text-center" style="position:absolute; padding:0;margin:0; top:0px; opacity:.3; z-index:-1 " alt="">
-        <div class="">
 
-            <!-- TOP -->
-            <div class="my-5 mb-2">
-                <img src="icon.png" class="img-fluid text-center d-block mx-auto" alt="">
+<body style="background-color:black;background-image:url('assets/pz_logo_nobg.png' no-repeat)">
+<!--img src='assets/pz_logo_nobg.png' class='logo-bg'/>
+-->
+<!-- GRANDE! sei un tipo sveglio,  vieni a leggere i commenti sul mio sito, ti becchi le cose in anteprima -->
+	<div class="text-center mt-5">
+		<a href="https://www.instagram.com/cyber.pz/" class='mx-4'><i class='bi-instagram' style='font-size:2rem;color:white'></i></a> 
+		<a href="https://discord.gg/RRMrZtAq" class='mx-4'><i class='bi-discord' style='font-size:2rem;color:white'></i></a> 
+		<a href="https://t.me/CyberPz" class='mx-4'><i class='bi-telegram' style='font-size:2rem;color:white'></i></a> 
+		<a href="https://www.linkedin.com/in/giuseppe-vitale-3b476b117/" class='mx-4'><i class='bi-linkedin' style='font-size:2rem;color:white'></i></a> 
+	</div> 
 
-                <div class="my-5 text-center">
-                    <h1 class="text-left" style="font-size: 3rem ;font-weight: bold; color:#00ACC1;">P ~ Z</h1>
-                </div>
+	<div class="container mt-5">
+			<div class="text-center d-grid gap-2 ">  
+			  <!--a href="https://www.twitch.tv/thezo_official" class="btn btn-primary btn-pz">Live VR Crypto Trading <i class='bi-twitch'></i></a-->
+				  <a href="https://github.com/cyberpz/" class="col mt-5 btn btn-primary btn-pz">GitHub <i class='bi-github'></i></a>
+				  <a href="https://discord.gg/RRMrZtAq" class="col-6 text-center btn btn-primary btn-pz">Cyber Space <i class='bi-discord'></i></a>
+			  <a href="https://www.youtube.com/channel/UCey78Bfv_Hr-OmSwbjzg0fA" class="btn btn-primary btn-pz">YouTube <i class='bi-youtube'></i></a>
 
-                <div class="text-center ">
-                    <a class="col-8 col-md-4 btn" style="border:none; border-radius:8px; color:#00Acc1" href="https://play.google.com/store/apps/details?id=svd.stp.manuel.oraristp&hl=en_US&gl=US"> <img src="assets/google-play-badge.png" class="img-fluid"> </a>
-                </div>
-            </div>
-
-            <!-- CODIN -->
-            <div class="text-center col-10 offset-1 my-5 p-5 " style="border-radius: 15px; border: 1px solid transparent; background-color:#00ACC1; box-shadow: 0px 0px 80px #00000029;">
-                <iframe width="100%" height="100" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/748104004&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-                    
-            </div>
-
-            <!-- MUSIC -->
-            <div class="text-center col-10 offset-1 my-5 p-5 d-md-flex " style="border-radius: 15px; border: 1px solid transparent; background-color:#d7f9fc; box-shadow: 0px 0px 80px #00000029;">
-                <div class="col-12 col-md-4 align-self-center mx-auto">
-                    <h3 class="col-12" style="color:#28c5d1; font-size:2rem"> Presenti su tutti i dispositivi.</h3>
-                    <p class="col-10 offset-1 text-center" style="color:#28c5d1">Con la nuova versione di Orari STP è possibile accedere trammite tutti i dispositivi. <br> A te la scelta </p>
-                </div>
-
-                <div class="col-12 col-md-6">
-                    <img src="assets/immagine.png" class="img-fluid" alt="">
-                </div>
-            </div>
-
-            <div class="text-center col-10 offset-1 my-5 p-5 " style="border-radius: 15px; border: 1px solid transparent; background-color:#00ACC1; box-shadow: 0px 0px 80px #00000029;">
-                <h3 class="pb-4" style="font-size:2rem; letter-spacing: 0px;">Il nostro Team.</h3>
-
-                <div class="row">
-                    <div class="col-12 col-md-4 mb-5  mt-3 mb-lg-0">
-                        <img class="bd-placeholder-img rounded-circle mb-3" width="140" height="140" src="assets/pz.jpeg" role="img" aria-label="" preserveAspectRatio="xMidYMid slice" focusable="false">
-
-                        <h2>Giuseppe Vitale </h2>
-                        <h6>@peppuz_</h6>
-                        <p> Fullstack developer</p>
-                        <div class="row mx-5" style="color:white">
-                            <a class="col" href="https://t.me/pz_boi"> <i style="color:white" data-feather="send"></i> </a>
-                            <a class="col" href="https://instagram.com/peppuz_"> <i style="color:white" data-feather="instagram"></i> </a>
-                            <a class="col" href="https://github.com/Peppuz/"> <i style="color:white" data-feather="github"></i> </a>
-                        </div>
-
-                    </div>
-                    <div class="col-12 col-md-4 mb-5 mt-3 mb-lg-0">
-                        <img class="bd-placeholder-img rounded-circle mb-3" width="140" height="140" src="assets/vorpal.jpeg" role="img" aria-label="" preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <h2> Manuel Manelli </h2>
-                        <h6>@vorpal_97</h6>
-                        <p>Backend developer</p>
-                        <div class="row mx-5" style="color:white">
-                            <a class="col" href="https://t.me/Vorpal_97"> <i style="color:white" data-feather="send"></i> </a>
-                            <a class="col" href="https://instagram.com/vorpal_97"> <i style="color:white" data-feather="instagram"></i> </a>
-                            <a class="col" href="https://github.com/Vorpal97"> <i style="color:white" data-feather="github"></i> </a>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4 mb-5 mt-3 mb-lg-0">
-                        <img class="bd-placeholder-img rounded-circle mb-3" width="140" height="140" src="assets/alberto.jpeg" role="img" aria-label="" preserveAspectRatio="xMidYMid slice" focusable="false">
-
-                        <h2>Alberto Carrone</h2>
-                        <h6>@albertocarrone</h6>
-                        <p>Designer</p>
-                        <div class="row mx-5 " style="color:white">
-                            <a class="col" href="https://t.me/albrtcr"> <i style="color:white" data-feather="send"></i> </a>
-                            <a class="col" href="https://instagram.com/albertocarrone"> <i style="color:white" data-feather="instagram"></i> </a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <div class="text-center col-10 offset-1 my-5 p-5 " style="border-radius: 15px; border: 1px solid transparent; background-color:#d7f9fc; box-shadow: 0px 0px 80px #00000029;">
-                <div>
-                    <h3 class="col-12" style="color:#28c5d1; font-size:2rem"> Hai una domanda da farci?</h3>
-                    <p class="col-10 offset-1 text-center" style="color:#28c5d1">Contattaci all'indirizzo di posta elettronica </p>
-                    <br>
-                    <br>
-                    <div style="display: flex;justify-content: center;">
-                        <div style="border:solid 1px #28c5d1; border-radius:15px; width: -moz-fit-content; width: fit-content;" class="p-3">
-                            <i style="color:#28c5d1; margin-right:5px; position:absolute" data-feather="mail"></i>
-                            <a style="color:#00ACC1; text-decoration:solid; margin-left:30" href="mailto:svdevteam72019@gmail.com">svdevteam72019@gmail.com</a>
-                        </div>
-                    </div>
-                    <br>
-                    <br>
-                    <p class="col-10 offset-1 text-center" style="color:#28c5d1">Attenzione, richieste riguardanti il servizio di trasporto verranno ignorate. <br>
-                        Contattate la società responsabile del servizio di trasporto pubblico.
-                    </p>
-                </div>
-
-                <div>
-                    <img src="" alt="">
-                </div>
-            </div>
-        </div>
+			  <a href="https://oraristp.it" class="mt-5 btn btn-primary btn-pz">OrariSTP 🚍- Website </a>
+			  <a href="https://www.brindisireport.it/cronaca/presentata-la-app-orari-stp-ideata-da-tre-studenti-di-san-vito-dei-normanni-brindisi.html" class="mb-5 btn btn-primary btn-pz">OrariSTP 🚍- Press - BrindisiReport</a>
 
 
+		<div class="container text-center mt-2">
+			<iframe class='mt-3' width="560" height="315" src="https://www.youtube-nocookie.com/embed/rgBIvIqWlwo?controls=0&amp;start=66" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+	</div> 
 
-        <label class="w-100 text-center" style="color:#b6edf1;font-weight:bold; font-size:1rem">POWERED BY SVDEVTEAM</label>
-        <br>
-        <br>
-        <br>
-    </div>
-    </div>
 
-    <script>
-        // icons replacing by feather
-        feather.replace()
-    </script>
+<!---
+			  <a href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/29249078072168079138729583986917553149873223026945245769999567076128723042305" class="btn btn-primary btn-pz">Life NFT - OpenSea <i class='bi-box-seam'></i></a>
 
+    <nft-card  contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e" tokenId="29249078072168079138729583986917553149873223026945245769999567076128723042305"> </nft-card> 
+<script src="https://unpkg.com/embeddable-nfts/dist/nft-card.min.js"></script>
+
+--->
+
+
+ <?php include 'spipa.php';?> 
 </body>
 
 </html>
